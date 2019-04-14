6fef9c6a371b6072769d79411fd499c65f2b3b7a
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-04-14 22:51:45 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,7 +4,7 @@ class Hash
   def keys_of(*arguments)
     keys = []
     self.each do |key, value|
-      #binding.pry
+      binding.pry
          if value = [value] == *arguments 
          #binding.pry  
           keys << key
